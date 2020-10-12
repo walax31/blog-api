a3c5bb79890f24552baf4df3e18f33ac6a074a40
walax31 <walax31009@gmail.com>
2020-10-12 15:59:03 +0700

    add function photo

--- src/config/pages/Story.js
+++ src/config/pages/Story.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import withHelmet from '../../utils/withHelmet'
+import withHelmet from '../utils/withHelmet'
 
+async function getPost (id) {
+  const response = await fetch.get(`http://localhost:3333/api/v1/posts/${id}`)
+  return response.json()
+}
+function usePostData (postId) {
+  const [post, setPost] = useState({})
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState(undefined)
+  async function reteiveData () {
+    try {
+      setLoading(true)
+      const postResponse = await getPost(postId)
+      setPost(postResponse)
+    } catch (err) {
+      setError(err)
+    }
+    setLoading(false)
+  }
+  return [{ post, isLoading, error }, { reteiveData }]
+}
 function Story () {
   const { storyId } = useParams()
-  return (
-    <div>Story page{storyId}
-
-    </div>
-  )
+  const [{ story, isLoading, error }, { reteiveData }] = usePostData(storyId)
+  useEffect(() => {
+    reteiveData()
+  }, [reteiveData])
+  if (isLoading) {
+    return <div>...loading</div>
+  }
+  return <div>Story page{storyId}</div>
 }
+
 export default withHelmet('Walauck | Blog')(Story)
